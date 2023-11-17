f291ecfe1ef5e245f785975a6966f3562f07ab44
geonryu <60060831+geonryu@users.noreply.github.com>
2023-11-17 22:21:42 +0900

    라우트세팅

---
+++ src/components/global/header.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components"
+import GlovalNavigation from "./nav";
+import { useState } from "react";
+import { Col, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = styled.header`
+    position: fixed; top: 0; left: 0; width: 100%; z-index: 99999; transition: all 0.4s; height: 66px;
+
+    & > .container {height: 65px;}
+    @media (min-width: 992px) {
+        overflow: hidden;
+
+        &.active{height: 260px}
+        & .sub{
+            display: block !important;
+            height: unset;
+        }
+        &::after{
+            // content: '';
+            display: block;
+            width: 100%;
+            height: 0;
+            transition: height 0.3s;
+            background-color: #fff;
+            // position: absolute;
+            top: 100%;
+            left: 0;
+        }
+        &.extend::after{
+            // border-bottom: 1px solid #dee2e6;
+            height: 200px;
+        }
+    }
+
+`;
+const HeaderWrap = styled.div``;
+const Logo = styled.h1``;
+const BtnNav = styled.div`
+    width: 35px; height: 35px;
+    & .navbar-toggler{
+        width: 100%; height: 100%; font-size: 0; position: relative;}
+    }
+    & .navbar-toggler::before{content: ''; display: block; width: 28px; height: 2px; background-color: #000; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -9px);}
+    & .navbar-toggler span{display: block; width: 28px; height: 2px; background-color: #000; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);}
+    & .navbar-toggler::after{content: ''; display: block; width: 28px; height: 2px; background-color: #000; position: absolute; top: 50%; left: 50%; transform: translate(-50%, 7px);}
+    & .navbar-toggler.active::before{top: calc(50% + 7px); transform: translate(-50%, -9px) rotate(-45deg); transition: top 0.15s 0.0s, transform 0.15s 0.15s;}
+    & .navbar-toggler.active span{display: none;}
+    & .navbar-toggler.active::after{top: calc(50% - 9px); transform: translate(-50%, 7px) rotate(45deg); transition: top 0.15s 0.0s, transform 0.15s 0.15s;}
+`;
+
+export default function GlovalHeader() {
+    const [navBtn, setNavBtn] = useState(false);
+    const onClickNavBtn = () => {
+        !navBtn ? setNavBtn(true) : setNavBtn(false);
+    }
+    
+    return (
+        <Header className={`header bg-primary-white border-bottom fixed-top ${navBtn ? "active" : "none"}`}>
+            <Container className="navbar-expand-lg navbar-light">
+                <HeaderWrap className="d-flex align-items-center align-items-lg-start justify-content-between h-100 w-100">
+                    <Col xs={5} md={3} lg={2} className="h-100">
+                        <Logo id="logo" className="mb-0 h-100 py-2 d-flex align-items-center">
+                            <Link to="/"><img className="h-100" src="https://firebasestorage.googleapis.com/v0/b/kcaar-dbf18.appspot.com/o/assets%2Fkcaar-logo.svg?alt=media&token=b49d8da2-7790-4c92-b523-23bba8c75a2d&_gl=1*1km8rig*_ga*ODgwNzAyNDA3LjE2OTI0NDA5MTM.*_ga_CW55HF8NVT*MTY5OTE2Njc4MC4xNy4xLjE2OTkxNjg0OTIuMzYuMC4w" alt="한국접근성평가연구원" /></Link>
+                        </Logo>
+                    </Col>
+                    <GlovalNavigation setHeaderStatus={setNavBtn} headerStatus={navBtn}></GlovalNavigation>
+                    <BtnNav className="btn-nav-opn d-lg-none">
+                        <button onClick={onClickNavBtn} className={`navbar-toggler border-0 px-1 py-2 ${navBtn ? "active" : "none"}`}>
+                            <span>메뉴열기/닫기</span>
+                        </button>
+                    </BtnNav>
+                </HeaderWrap>
+            </Container>
+        </Header>
+    )
+}
---
+++ src/components/home/mainVisual.tsx
@@ -0,0 +1,83 @@
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from "swiper/modules";
+import { SwiperSlide, Swiper } from "swiper/react";
+
+const Section = styled.section``;
+const SliderContent = styled.div`
+    height: 430px;
+`;
+const Next = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+`;
+const Prev = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+`;
+
+export default function MainVisual() {
+    return (
+        <Section className="py-5">
+            <Container>
+                <Swiper
+                    loop={true}
+                    slidesPerView={1}
+                    modules={[Navigation]}
+                    className="overflow-hidden position-relative"
+                    speed={600}
+                    slideToClickedSlide={true}
+                    spaceBetween={30}
+                    navigation={
+                        {
+                            nextEl: ".nextBtn",
+                            prevEl: ".prevBtn",
+                        }
+                    }
+                    // breakpoints={{
+                    //     768: {
+                    //       slidesPerView: 3,
+                    //       spaceBetween: 30,
+                    //     },
+                    //     992: {
+                    //       slidesPerView: 4,
+                    //       spaceBetween: 30,
+                    //     },
+                    // }}
+                >
+                    <SwiperSlide>
+                        <SliderContent className="slide-inner px-3 px-md-5 py-5 bg-primary rounded-4 overflow-hidden">
+                            <div className="text-content px-3 px-md-5 px-md-5 rounded d-flex flex-column justify-content-end h-100">
+                                <div className="tit fw-bold text-primary-white fs-3 pt-4 px-md-2 mb-3">우선구매대상 지능정보제품<br /> 국내 1호 시험평가기관</div>
+                                <div className="txt fx-5 text-primary-white px-md-2">한국접근성평가연구원은 2022년 한국지능정보사회진흥원의 우선구매 대상 지능정보제품(키오스크) 시험평가기관으로 공식 선정되었으며 국내 최초로 키오스크 시험평가를 진행한 기관입니다.</div>
+                            </div>
+                        </SliderContent>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <SliderContent className="slide-inner px-3 px-md-5 py-5 bg-primary rounded-4 overflow-hidden">
+                            <div className="text-content px-3 px-md-5 px-md-5 rounded d-flex flex-column justify-content-end h-100">
+                                <div className="tit fw-bold text-primary-white fs-3 pt-4 px-md-2 mb-3">모두를 위한 디자인<br />유니버설(접근성) 디자인의 선구자</div>
+                                <div className="txt fx-5 text-primary-white px-md-2">성균관대학교 시스템경영공학자 교수이자 (사)한국접근성평가연구원 이사장인 이성일 교수는 장애인 및 고령자 삶의 질 향상을 위한 기술 개발 및 접근성의 표준화를 연구하고 있으며, 유니버설 디자인과 정보통신 접근성 관련 활동을 활발히 진행중입니다.</div>
+                            </div>
+                        </SliderContent>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <SliderContent className=".slide-inner px-3 px-md-5 py-5 bg-primary rounded-4 overflow-hidden">
+                            <div className="text-content px-3 px-md-5 px-md-5 rounded d-flex flex-column justify-content-end h-100">
+                                <div className="tit fw-bold text-primary-white fs-3 pt-4 px-md-2 mb-3">LG전자 키오스크 평가기관</div>
+                                <div className="txt fx-5 text-primary-white px-md-2">한국접근성평가연구원은 우선구매대상 지능정보제품 검증서를 취득한 LG전자 키오스크 제품의 국내 최초 시험평가 기관입니다.</div>
+                            </div>
+                        </SliderContent>
+                    </SwiperSlide>
+                    <Next className="nextBtn slider-navigation text-primary-white d-none d-md-flex align-items-center pe-4"><div className="d-md-flex align-items-center justify-content-center"><span className="material-symbols-outlined fw-bold fs-4 d-block">chevron_right</span></div></Next>
+                    <Prev className="prevBtn slider-navigation text-primary-white d-none d-md-flex align-items-center ps-4"><div className="d-md-flex align-items-center justify-content-center"><span className="material-symbols-outlined fw-bold fs-4 d-block">chevron_left</span></div></Prev>
+                </Swiper>
+            </Container>
+        </Section>
+    )
+}
