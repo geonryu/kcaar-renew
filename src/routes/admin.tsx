aeae89d47da8037d329b13dfc2cd643a9ab32d56
geonryu <60060831+geonryu@users.noreply.github.com>
2023-11-19 21:12:09 +0900

    test

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,36 +1,34 @@
 import { Navigate } from "react-router-dom";
-import { auth, db } from "../firebase";
+import { db } from "../firebase";
 import { useEffect, useState } from "react";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
 export default function Admin({children} : {children:React.ReactNode}) {
     const [perm, setPerm] = useState<any>(false);
-    const [user, setUser] = useState<any>(() => {
+    const user = useState<any>(() => {
         const storedData = localStorage.getItem('cur');
         return storedData ? JSON.parse(storedData) : false;
     });
-   
 
-    useEffect(() => {
-        const fetchUser = async() => {
-            try {
-                const collectionRef = collection(db, 'users');
-                const userQuery = query(collectionRef, where('uid', '==', user.uid));
-                const querySnapshot = await getDocs(userQuery);
-                if (!querySnapshot.empty) {
-                    const result = querySnapshot.docs[0].data();
-                    setPerm(result.permission);
-                    console.log(perm);
-                }
-            } catch (error) {
-                console.error('Permission denied');
+    const fetchUser = async() => {
+        try {
+            const collectionRef = collection(db, 'users');
+            const userQuery = query(collectionRef, where('uid', '==', user[0].uid));
+            const querySnapshot = await getDocs(userQuery);
+            if (!querySnapshot.empty) {
+                const result = querySnapshot.docs[0].data();
+                setPerm(result.permission);
+            }
+        } catch (error) {
+            console.error('Permission denied');
+        } finally {
+            console.log(perm);
+            if(!perm || !user) {
+                return <Navigate to="/" />
             }
             
+            return children;
         }
-        fetchUser();
-    }, []);
-    if(!perm || !user) {
-        return <Navigate to="/" />
     }
-    return children;
+    fetchUser();
 }
